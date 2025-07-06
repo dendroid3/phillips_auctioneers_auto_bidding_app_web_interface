14adb0b6063d506abbe4c56d9e9e94082f3fa61c
Wanjohi <kazibin66@gmail.com>
2025-07-06 23:48:30 +0300

    Whatever

--- app/Http/Resources/VehicleResource.php
+++ app/Http/Resources/VehicleResource.php
@@ -4,7 +4,7 @@ namespace App\Http\Resources;
 
 use App\Models\BidStage;
 use Illuminate\Http\Resources\Json\JsonResource;
-
+use App\Models\Bid;
 class VehicleResource extends JsonResource
 {
     public function toArray($request)
